b37ed6b2524407148f12e5ed585ed18c6a3ba836
nicolas_lozano7@hotmail.com <nicolas_lozano7@hotmail.com>
2023-08-29 10:52:22 -0300

    Cliente preparado para conexion. falta agregar servicios

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:9090");
+            var client = new receta.recetaClient(channel);
+
+        }
+    }
+}
