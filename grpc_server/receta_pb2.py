0f0757fa06372d1b1a5f754924c07abd61c60e7c
Matias Silvestri <71100012+matiasFS@users.noreply.github.com>
2023-09-02 19:13:30 -0300

    Actualizados los archivos generados por proto

--- grpc_server/receta_pb2.py
+++ grpc_server/receta_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0creceta.proto\"\xcc\x01\n\x06Receta\x12\x10\n\x08idreceta\x18\x01 \x01(\x05\x12\x0e\n\x06titulo\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scripcion\x18\x03 \x01(\t\x12\x19\n\x11tiempoPreparacion\x18\x04 \x01(\x05\x12\x14\n\x0cingredientes\x18\x05 \x01(\t\x12\r\n\x05pasos\x18\x06 \x01(\t\x12\x11\n\turl_fotos\x18\x07 \x03(\t\x12\x19\n\x11usuario_idusuario\x18\x08 \x01(\x05\x12\x1d\n\x15\x63\x61tegoria_idcategoria\x18\t \x01(\x05\".\n\tResponsea\x12\x0f\n\x07message\x18\x01 \x01(\t\x12\x10\n\x08idreceta\x18\x02 \x01(\x05\x32,\n\x07Recetas\x12!\n\nAltaReceta\x12\x07.Receta\x1a\n.Responseab\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0creceta.proto\"\xcc\x01\n\x06Receta\x12\x10\n\x08idreceta\x18\x01 \x01(\x05\x12\x0e\n\x06titulo\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scripcion\x18\x03 \x01(\t\x12\x19\n\x11tiempoPreparacion\x18\x04 \x01(\x05\x12\x14\n\x0cingredientes\x18\x05 \x01(\t\x12\r\n\x05pasos\x18\x06 \x01(\t\x12\x11\n\turl_fotos\x18\x07 \x03(\t\x12\x19\n\x11usuario_idusuario\x18\x08 \x01(\x05\x12\x1d\n\x15\x63\x61tegoria_idcategoria\x18\t \x01(\x05\"\x0c\n\nNuloReceta\".\n\tResponsea\x12\x0f\n\x07message\x18\x01 \x01(\t\x12\x10\n\x08idreceta\x18\x02 \x01(\x05\"&\n\nRecetaList\x12\x18\n\x07recetas\x18\x01 \x03(\x0b\x32\x07.Receta2T\n\x07Recetas\x12!\n\nAltaReceta\x12\x07.Receta\x1a\n.Responsea\x12&\n\x0cTraerRecetas\x12\x0b.NuloReceta\x1a\x07.Receta0\x01\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -23,8 +23,12 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
   _globals['_RECETA']._serialized_start=17
   _globals['_RECETA']._serialized_end=221
-  _globals['_RESPONSEA']._serialized_start=223
-  _globals['_RESPONSEA']._serialized_end=269
-  _globals['_RECETAS']._serialized_start=271
-  _globals['_RECETAS']._serialized_end=315
+  _globals['_NULORECETA']._serialized_start=223
+  _globals['_NULORECETA']._serialized_end=235
+  _globals['_RESPONSEA']._serialized_start=237
+  _globals['_RESPONSEA']._serialized_end=283
+  _globals['_RECETALIST']._serialized_start=285
+  _globals['_RECETALIST']._serialized_end=323
+  _globals['_RECETAS']._serialized_start=325
+  _globals['_RECETAS']._serialized_end=409
 # @@protoc_insertion_point(module_scope)
--- grpc_server/receta_pb2_grpc.py
+++ grpc_server/receta_pb2_grpc.py
@@ -19,12 +19,23 @@ class RecetasStub(object):
                 request_serializer=receta__pb2.Receta.SerializeToString,
                 response_deserializer=receta__pb2.Responsea.FromString,
                 )
+        self.TraerRecetas = channel.unary_stream(
+                '/Recetas/TraerRecetas',
+                request_serializer=receta__pb2.NuloReceta.SerializeToString,
+                response_deserializer=receta__pb2.Receta.FromString,
+                )
 
 
 class RecetasServicer(object):
     """Missing associated documentation comment in .proto file."""
 
     def AltaReceta(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def TraerRecetas(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
@@ -37,6 +48,11 @@ def add_RecetasServicer_to_server(servicer, server):
                     servicer.AltaReceta,
                     request_deserializer=receta__pb2.Receta.FromString,
                     response_serializer=receta__pb2.Responsea.SerializeToString,
+            ),
+            'TraerRecetas': grpc.unary_stream_rpc_method_handler(
+                    servicer.TraerRecetas,
+                    request_deserializer=receta__pb2.NuloReceta.FromString,
+                    response_serializer=receta__pb2.Receta.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -62,5 +78,22 @@ class Recetas(object):
         return grpc.experimental.unary_unary(request, target, '/Recetas/AltaReceta',
             receta__pb2.Receta.SerializeToString,
             receta__pb2.Responsea.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def TraerRecetas(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_stream(request, target, '/Recetas/TraerRecetas',
+            receta__pb2.NuloReceta.SerializeToString,
+            receta__pb2.Receta.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
