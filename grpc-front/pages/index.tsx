2e913d8ebc5708e6f02c2fd5ee359208802dfc1c
nicolas_lozano7@hotmail.com <nicolas_lozano7@hotmail.com>
2023-09-02 15:45:04 -0300

    Subida front end con tabla y home

---
+++ grpc-front/pages/index.tsx
@@ -0,0 +1,19 @@
+import { title, subtitle } from "@/components/primitives";
+import TableComponent from "@/components/table";
+import DefaultLayout from "@/layouts/default";
+import { Spacer } from "@nextui-org/react";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title({color: "blue"})}>Recetas creadas por la  &nbsp;</h1>
+          <h1 className={title({ color: "green" })}>Comunidad&nbsp;</h1>
+       </div>
+      <Spacer/>
+      <TableComponent/>
+      </section>
+    </DefaultLayout>
+  );
+}
